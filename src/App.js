f207c7b3fe85c9ec1313f93d6e0a669c385f0ce7
syahraazly <73158627+Syahra-Zulya-Shania-Maghfiroh@users.noreply.github.com>
2023-07-18 09:36:37 +0700

    simple card

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+console.log(AlexaImage, CortanaImage, SiriImage);
 
 function App() {
   return (
     <div>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code> src / App.js </code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistant</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} description="Alexa wascreated by Amazon helps you buy things" />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="Cortana was made by Microsoft. Who knows what it does?"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Siri" handle="@siri01" image={SiriImage} description="Siri wasmade by Apple and is being phased out" />
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
 }
-
 export default App;
