f019e7399201c1462b3110e1f35e31a5308b5a32
syahraazly <73158627+Syahra-Zulya-Shania-Maghfiroh@users.noreply.github.com>
2023-07-24 15:47:42 +0700

    refactoring code using the context system

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,39 +1,40 @@
-import { useState } from "react";
-import BookEdit from './BookEdit';
+import { useState, useContext } from "react";
+import BooksContext from "../context/books";
+import BookEdit from "./BookEdit";
 
-function BookShow({ book, onDelete, onEdit }) {
+function BookShow({ book }) {
   const [showEdit, setShowEdit] = useState(false);
+  const { deleteBookById } = useContext(BooksContext);
 
   const handleDeleteClick = () => {
-    onDelete(book.id);
+    deleteBookById(book.id);
   };
 
   const handleEditClick = () => {
     setShowEdit(!showEdit);
   };
 
-  const handleSubmit = (id, newTitle) => {
+  const handleSubmit = () => {
     setShowEdit(false);
-    onEdit(id, newTitle);
-  }
+  };
 
-  let content = <h3>{book.title}</h3>;
+  let content = <h3> {book.title} </h3>;
   if (showEdit) {
-    content = <BookEdit onSubmit={handleSubmit} onEdit={onEdit} book={book}/>
+    content = <BookEdit onSubmit={handleSubmit} book={book} />;
   }
 
   return (
     <div className="book-show">
       <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="books" />
-      <div>{content}</div>
+      <div> {content} </div>{" "}
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
-          Edit
-        </button>
+          Edit{" "}
+        </button>{" "}
         <button className="delete" onClick={handleDeleteClick}>
-          Delete
-        </button>
-      </div>
+          Delete{" "}
+        </button>{" "}
+      </div>{" "}
     </div>
   );
 }
